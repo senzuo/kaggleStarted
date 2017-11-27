64f562c01e3fcfb36fac026568ae0ede40e3bb4e
senzuo <1327336044@qq.com>
2017-11-27 14:58:17 +0800

    //

--- titanic/src/MykNN.py
+++ titanic/src/MykNN.py
@@ -1,22 +1,34 @@
 # -*- coding:utf8 -*-
 
+import math
+
+
 # 加载数据
 def loadData(fileName):
     trainSet = []
-    # with open(fileName) as f:
-    f = open(fileName)
-    for line in f.readlines():
-        line = line.strip().split(',')
-        trainSet.append(line)
-    featurn = trainSet.pop(0)
+    with open(fileName) as f:
+    # without close
+    # f = open(fileName)
+        for line in f.readlines():
+            line = line.strip().split(',')
+            trainSet.append(line)
+        featurn = trainSet.pop(0)
     return featurn, trainSet
 
+# 数值化
+def numberize():
+    pass
+
+# 归一化
+
 # 距离
-def distance(vecA, VecB):
+def distance(vecA, vecB):
+    return math.sqrt(sum(((vecA - vecB)**2)))
 
 
-def kNN(k)
+def kNN(k):
+    pass
 
 if __name__ == '__main__':
     featurn, trainSet = loadData('../data/train.csv')
-    print(trainSet)
+    print(featurn)
